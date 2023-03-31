0177e5da1434bc0d8fd24f1683fb891c1fe2b9e9
LJTKirito <129251472+LJTKirito@users.noreply.github.com>
2023-03-31 17:42:22 +0800

    Add files via upload
    采用二维数组实现代数里矩阵的乘法

---
+++ Untitled22-二维数组实现矩阵的乘法.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int arr1[3][4],arr2[4][3],arr[3][3],i,j,n,s=0;
+	for(i=0;i<3;i++){
+		for(j=0;j<4;j++){
+		arr1[i][j]=rand()%10;
+		printf("%d ",arr1[i][j]);		 
+		}
+		printf("\n"); 
+	}
+	printf("\n");
+	for(i=0;i<4;i++){
+		for(j=0;j<3;j++){
+		arr2[i][j]=rand()%10;
+		printf("%d ",arr2[i][j]);		 
+		}
+		printf("\n"); 
+	}
+	printf("\n");
+	for(i=0;i<3;i++){
+		for(j=0;j<3;j++){
+			for(n=0;n<4;n++){
+				s+=arr1[j][n]*arr2[n][j]; //arr[i][j]是一个常量；arr[i][j]一旦赋值不能变化； 
+			}
+			arr[i][j]=s;
+			printf("%d ",arr[i][j]);
+		}
+		printf("\n");
+	}
+ } 
